ab29e9a84199f07f6089c0bbd2aee85ed67391bf
zhangkai <zkdtckk@gmail.com>
2020-05-14 21:34:16 -0700

    add listner to first search button

--- script.js
+++ script.js
@@ -8,3 +8,27 @@ $(document).ready(function() {
       $valueSpan.html($value.val());
     });
   });
+  
+  $('#search-btn1').on("click",function(event){
+    event.preventDefault();
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?timeFrame=day&targetCalories=2000&diet=vegetarian&exclude=shellfish%252C%20olives",
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        "x-rapidapi-key": "69873bfbe3mshc65b70ea31b76c8p14c4e2jsn321f5f41b73a"
+      }
+    }
+    
+    $.ajax(settings).done(function (response) {
+      for (i=0;i<3;i++){
+        var title=response.meals[i].title;
+        var url=response.meals[i].sourceUrl;
+        $('#')
+      }
+
+    });
+
+  })
