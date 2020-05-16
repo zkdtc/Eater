d1c045836c730cd6e15a79bb26079f74844f96a3
zhangkai <zkdtckk@gmail.com>
2020-05-16 14:02:46 -0700

    New API

--- script.js
+++ script.js
@@ -13,18 +13,18 @@ $(document).ready(function () {
 // Add listner to the 'Search for Recipe button
 $('#search-btn1').on("click", function (event) {
   event.preventDefault();
-  retreiveRecipieGivenCalorie();
+  retreiveRecipie();
 })
 
 
-function retreiveRecipieGivenCalorie() {
+function retreiveRecipie() {
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/mealplans/generate?targetCalories=" + $('#slider1').val() + "&timeFrame=day",
+    "url": "https://recipe-puppy.p.rapidapi.com/?p=1&q="+$('#recipe-value').val(),
     "method": "GET",
     "headers": {
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+      "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
       "x-rapidapi-key": "69873bfbe3mshc65b70ea31b76c8p14c4e2jsn321f5f41b73a"
     }
   }
@@ -32,33 +32,16 @@ function retreiveRecipieGivenCalorie() {
   // First retrive three meals
 
   $.ajax(settings).done(function (response) {
-    console.log(response);
+    response=JSON.parse(response);
 
 
     for (i = 0; i < 3; i++) {
-      var title = response.meals[i].title;
-      var url = response.meals[i].sourceUrl;
-      var id = response.meals[i].id;
+      var title = response.results[i].title;
+      var url = response.results[i].href;
+      var img = response.results[i].thumbnail;
       $('#recipe-title' + String(i + 1)).text(title);
       $('#recipe-btn' + String(i + 1)).attr('href', url);
-      // Retrieve recipe summaries for each meal
-      var settings = {
-        "async": false,
-        "crossDomain": true,
-        "url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+id+"/summary",
-        "method": "GET",
-        "headers": {
-          "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-          "x-rapidapi-key": "69873bfbe3mshc65b70ea31b76c8p14c4e2jsn321f5f41b73a"
-        }
-      }
-
-      $.ajax(settings).done(function (response2) {
-         console.log(response2);
-         var summary=response2.summary;
-         $('#recipe-description'+String(i+1)).html(summary);
-        
-      });
+      $('#img'+String(i+1)).attr('src',img);
 
     }
 
