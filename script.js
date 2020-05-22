e3ce26fc7b9c10a1a1da5acf01d113b7c8ea6428
zhangkai <zkdtckk@gmail.com>
2020-05-21 19:23:49 -0700

    Working version

--- script.js
+++ script.js
@@ -44,9 +44,10 @@ function retreiveRecipie() {
 
 
     for (i = 0; i < 3; i++) {
-      var title = response.results[i].title;
-      var url = response.results[i].href;
-      var img = response.results[i].thumbnail;
+      vindex=Math.floor(Math.random()*response.results.length);
+      var title = response.results[index].title;
+      var url = response.results[index].href;
+      var img = response.results[index].thumbnail;
       $('#recipe-title' + String(i + 1)).text(title);
       $('#recipe-btn' + String(i + 1)).attr('href', url);
       $('#img' + String(i + 1)).attr('src', img);
@@ -55,9 +56,79 @@ function retreiveRecipie() {
 };
 
 
+// Add listner to the 'Search for Recipe button
+$('#search-btn2').on("click", function (event) {
+  event.preventDefault();
+  retreiveRecipieByIngredients();
+})
 
 
+function retreiveRecipieByIngredients() {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + $('#ingredients-value').val(),
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+      "x-rapidapi-key": "69873bfbe3mshc65b70ea31b76c8p14c4e2jsn321f5f41b73a"
+    }
+  }
 
+  // First retrive three meals
+
+  $.ajax(settings).done(function (response) {
+    response = JSON.parse(response);
+    console.log(response);
+
+    for (i = 0; i < 3; i++) {
+      index=Math.floor(Math.random()*response.results.length);
+      var title = response.results[index].title;
+      var url = response.results[index].href;
+      var img = response.results[index].thumbnail;
+      $('#recipe-title' + String(i + 1)).text(title);
+      $('#recipe-btn' + String(i + 1)).attr('href', url);
+      $('#img' + String(i + 1)).attr('src', img);
+    }
+  })
+};
+
+// Add listner to the 'Search for Recipe button
+$('#search-btn3').on("click", function (event) {
+  event.preventDefault();
+  retreiveRecipieByBoth();
+})
+
+
+function retreiveRecipieByBoth() {
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://recipe-puppy.p.rapidapi.com/?p=1&i=" + $('#ingredients-value').val()+"&q=" + $('#recipe-value').val(),
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+      "x-rapidapi-key": "69873bfbe3mshc65b70ea31b76c8p14c4e2jsn321f5f41b73a"
+    }
+  }
+
+  // First retrive three meals
+
+  $.ajax(settings).done(function (response) {
+    response = JSON.parse(response);
+    console.log(response);
+
+    for (i = 0; i < 3; i++) {
+      index=Math.floor(Math.random()*response.results.length);
+      var title = response.results[index].title;
+      var url = response.results[index].href;
+      var img = response.results[index].thumbnail;
+      $('#recipe-title' + String(i + 1)).text(title);
+      $('#recipe-btn' + String(i + 1)).attr('href', url);
+      $('#img' + String(i + 1)).attr('src', img);
+    }
+  })
+};
 
 
 
